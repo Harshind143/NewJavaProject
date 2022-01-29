5c9c9e5ac817013ccd10172e38342db7ed820a3a
harshalshinde143@gmail.com <harshalshinde143@gmail.com>
2022-01-29 21:43:36 +0530

    Test

---
+++ src/MarksDemo.java
@@ -0,0 +1,18 @@
+public class MarksDemo {
+
+    public static void main(String[] args) {
+        int marks = 100;
+
+        if (marks < 40){
+            System.out.println("failed");
+        }else if(marks>=40 && marks <50){
+            System.out.println("Passed");
+        }else if(marks >=50 && marks <60){
+            System.out.println("Second Class");
+        }else if (marks>=60 && marks< 66){
+            System.out.println("first class");
+        }else if (marks>=66 && marks<=100){
+            System.out.println("Distinction");
+        }
+    }
+}
