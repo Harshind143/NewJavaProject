58ef312e8940d89d648fb41541d00c553ed2a305
harshalshinde143@gmail.com <harshalshinde143@gmail.com>
2022-02-02 21:21:08 +0530

    Test

---
+++ src/Increment_decrementDemo.java
@@ -0,0 +1,12 @@
+public class Increment_decrementDemo {
+    public static void main(String[] args) {
+        int i =10;
+
+        System.out.println(i);
+        System.out.println(i++);
+        System.out.println(i);
+        System.out.println(++i);
+        System.out.println(i);
+
+    }
+}
