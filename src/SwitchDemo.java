e5d7ffdf36eed03df3661a4a1194916b648aec01
harshalshinde143@gmail.com <harshalshinde143@gmail.com>
2022-02-02 21:03:50 +0530

    Test

---
+++ src/SwitchDemo.java
@@ -0,0 +1,28 @@
+public class SwitchDemo {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b =2;
+        int c;
+        String operation ="div";
+
+        switch (operation)
+        {
+            case "add" : c = a+b;
+                System.out.println("addition "+ c);
+                break;
+            case "sub"  : c=a-b;
+                System.out.println("subtraction "+ c);
+                break;
+            case "mul" : c= a*b;
+                System.out.println("Multiplication "+c);
+                break;
+            case "div" : c=a/b;
+                System.out.println("Division "+ c);
+                break;
+            default:
+                System.out.println("not able to do the operation");
+
+        }
+    }
+}
---
+++ src/triangleTest.java
@@ -0,0 +1,28 @@
+public class triangleTest {
+
+    public static void main(String [] args) {
+        int a = 25;
+        int b = 90;
+        int c = 65;
+
+        if (a + b + c == 180 && a>0 && b>0 && c>0) {
+
+            if (a == b && b == c) {
+                System.out.println("equilateral tringel");
+            }
+            if (a != b && b != c && c != b) {
+                System.out.println("scalen triangle");
+            }
+            if (a == 90 | b == 90 | c == 90) {
+                System.out.println("Right angel tringle");
+
+            }
+            if ((a == b && c != b) || (b == c && a != c) || (c == a && b != a)) {
+                System.out.println("Isoscalen triangle");
+            }
+        }else{
+            System.out.println("this is not triangle");
+        }
+    }
+
+}
